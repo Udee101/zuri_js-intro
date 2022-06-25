37950228fc4a594b99ba58b41e066715b934f071
Udeorah Frank <franklynudeorah@yahoo.com>
2022-06-25 11:56:49 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const fullName = 'Frank Udeorah';
+const height = '5.9ft';
+const country = 'Nigreria';
+
+document.write('<b>Name:</b> ',fullName, '<br> <b>Height:</b> ',height, '<br> <b>Country:</b> ',country);
